54137e92c28780f46305f1d9fce03c05fc4f2562
Lim Ting Wei <tingwei3931@gmail.com>
2017-11-10 16:49:28 +0800

    First commit

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ require('dotenv').config();
 const restify = require('restify');
 //part of bot framework used to create the bot
 const builder = require('botbuilder');
+//ticket api to save ticket
+const ticketsApi = require('./ticketsApi');
+const fs = require('fs');
+
+
+
+const listenPort = process.env.port || process.env.PORT || 3978;
+const ticketSubmissionUrl = process.env.TICKET_SUBMISSION_URL || `http://localhost:${listenPort}`;
 
 // Setup Restify Server
 var server = restify.createServer();
+
+
+// Setup body parser and tickets api
+server.use(restify.bodyParser());
+server.post('/api/tickets', ticketsApi);
+
+
 // Listen to port
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log('%s listening to %s', server.name, server.url);
+server.listen(listenPort, '::', () => {
+    console.log('Server Up');
 });
+
+
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
@@ -22,8 +39,104 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, [
+var bot = new builder.UniversalBot(connector, (session) => {
+    session.endDialog(`I'm sorry, I did not understand '${session.message.text}'.\nType 'help' to know more about me :)`);
+});
+
+bot.dialog('SubmitTicket', [
     (session, args, next) => {
-        session.send('You said: ' + session.message.text + ' which was ' + session.message.text.length + ' characters');
+        var category = builder.EntityRecognizer.findEntity(args.intent.entities, 'category');
+        var severity = builder.EntityRecognizer.findEntity(args.intent.entities, 'severity');
+    
+        if (category && category.resolution.values.length > 0) {
+            session.dialogData.category = category.resolution.values[0];
+        }
+    
+        if (severity && severity.resolution.values.length > 0) {
+            session.dialogData.severity = severity.resolution.values[0];
+        }
+    
+        session.dialogData.description = session.message.text;
+    
+        if (!session.dialogData.severity) {
+            var choices = ['high', 'normal', 'low'];
+            builder.Prompts.choice(session, 'Which is the severity of this problem?', choices, { listStyle: builder.ListStyle.button });
+        } else {
+            next();
+        }
+    },
+    (session, result, next) => {
+        if (!session.dialogData.severity) {
+            session.dialogData.severity = result.response.entity;
+        }
+    
+        if (!session.dialogData.category) {
+            builder.Prompts.text(session, 'Which would be the category for this ticket (software, hardware, network, and so on)?');
+        } else {
+            next();
+        }
+    },
+    (session, result, next) => {
+        if (!session.dialogData.category) {
+            session.dialogData.category = result.response;
+        }
+        var message = `Great! I'm going to create a "${session.dialogData.severity}" severity ticket in the "${session.dialogData.category}" category. ` +
+                  `The description I will use is "${session.dialogData.description}". Can you please confirm that this information is correct?`;
+
+        builder.Prompts.confirm(session, message, { listStyle: builder.ListStyle.button });
+    },
+    (session, result, next) => {
+        console.log(result);
+        if (result.response) {
+            var data = {
+                category: session.dialogData.category,
+                severity: session.dialogData.severity,
+                description: session.dialogData.description,
+            }
+            
+            const client = restify.createJsonClient({ url: ticketSubmissionUrl });
+            client.post('/api/tickets', data, (err, request, response, ticketId) => {
+                if (err || ticketId == -1) {
+                    session.send('Something went wrong while I was saving your ticket. Please try again later.')
+                } else {
+                    session.send(new builder.Message(session).addAttachment({
+                        contentType: "application/vnd.microsoft.card.adaptive",
+                        content: createCard(ticketId, data)
+                    }));
+                }
+                
+            });
+        } else {
+            session.endDialog('Ok. The ticket was not created. You can start again if you want.');
+        }
     }
-]);
+]).triggerAction({
+    matches: 'SubmitTicket'
+});
+
+
+bot.dialog('Help',
+(session, args, next) => {
+    session.endDialog(`I'm the help desk bot and I can help you create a ticket.\n` +
+        `You can tell me things like _I need to reset my password_ or _I cannot print_.`);
+}
+).triggerAction({
+matches: 'Help'
+});
+
+var luisRecognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL).onEnabled(function (context, callback) {
+    var enabled = context.dialogStack().length === 0;
+    callback(null, enabled);
+});
+bot.recognizer(luisRecognizer);
+
+const createCard = (ticketId, data) => {
+    var cardTxt = fs.readFileSync('./cards/ticket.json', 'UTF-8');
+
+    cardTxt = cardTxt.replace(/{ticketId}/g, ticketId)
+                    .replace(/{severity}/g, data.severity)
+                    .replace(/{category}/g, data.category)
+                    .replace(/{description}/g, data.description);
+
+    return JSON.parse(cardTxt);
+};
